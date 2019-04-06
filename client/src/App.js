68306305321b6e9f360d906af7ec981db1db9c4e
David Sarkisian <dsarkisian@plansource.com>
2019-04-06 12:26:02 -0600

    Added no match file, ViewProduct component, added some routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, } from 'react-router-dom';
+import DepartmentList from './components/departments/DepartmentList';
+import Department from './components/departments/Derpartment';
+import ProductList from './components/products/ProductList';
+import Product from './components/products/Product';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Route exact path="/" component={DepartmentList} />
+    <Route exact path="/departments/:id" component={Department} />
+    <Route exact path="/department/:department_id/products" component={ProductList} />
+    <React exact path="/department/:department_id/products/:id" component={Product} />
+    <Route component={NoMatch} />
+  </>
+);
 
 export default App;
